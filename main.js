86a351bd983a9f609abbe9601b1645ccc10e551d
aahillohiya <120379091+aahillohiya@users.noreply.github.com>
2023-01-26 12:52:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var B_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	b_imgTag = new Image(); //defining a variable with a new image
+	b_imgTag.onload = uploadimg; // setting a function, onloading this variable
+	b_imgTag.src = B_image;   // load image
+}
+
+function uploadimg() {
+
+	ctx.drawImage(b_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)){
+		alphabetkey();
+		document.getElementById("d1").innerHTML="You pressed Alphabet Key"; 
+		console.log("alphabet key");
+		}
+		else if ((keyPressed >=48 && keyPressed<=57)) {
+		numberkey();
+		document.getElementById("d1").innerHTML="You pressed Number Key"
+		console.log("numder key")
+		}
+		else if ((keyPressed >=37 && keyPressed<=40)) {
+		Arrkey();
+		document.getElementById("d1").innerHTML="You pressed Arrow Key"
+		console.log("Arrow key")
+		}
+		else if ((keyPressed == 17) || (keyPressed == 18) || (keyPressed == 27)) {
+		specialkey();
+		document.getElementById("d1").innerHTML="You pressed Special Key"
+		console.log("special key")
+		}
+		else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+
+}
+
+function alphabetkey()
+{
+	B_image="Alpkey.png";
+	add();
+}
+
+function numberkey()
+{
+	B_image="numkey.png";
+	add();
+}
+
+function arrowkey()
+{
+	B_image="Arrkey.png";
+	add();
+}
+
+function specialkey()
+{
+	B_image="spkey.png";
+	add();
+}
+
+function otherkey()
+{
+	B_image="otherkey.png";
+	add();
+}
+	
